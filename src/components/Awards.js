4c077f8ef3bbc0e265173323f893f8dc887d01f0
naveengitboi <naveenjangiti9@gmail.com>
2023-08-07 22:08:44 +0530

    awards page done

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import '../cssComponents/Awards.css'
+
+
+
 function Awards() {
+  const awards = [
+    {
+      awardImg:"./images/adage.png",
+      text:"Design And Branding"
+    },
+     {
+      awardImg:"./images/webbyawards.png",
+      text:"Agency of the Year"
+    },
+     {
+      awardImg:"./images/campaign.png",
+      text:`Digital Innovation 
+      agency of the year finalist`
+    },
+  ]
   return (
-    <div className='awardsSection'>Awards</div>
+    <div className='awardsSection'>
+      {
+        awards.map((item, index) => (
+          <div className="award">
+            <img src={item.awardImg} alt="" />
+            <p className='smallPara'>{item.text}</p>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
--- src/components/HomeVideo.js
+++ src/components/HomeVideo.js
@@ -1,26 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import '../cssComponents/HomeVideo.css'
 import videoBg from '../assets/mainVideo.mp4'
 import { useState } from 'react'
 
 
 function HomeVideo() {
+  const cursorSec = useRef()
   const [mouseEnter, setMouseEnter] = useState({
     top:0,
     left:0
   })
+  const [mouseLeave, setMouseLeave] = useState(false)
 
 
   const handleMousePostion = (e) => {
-   setMouseEnter({
+   cursorSec.current.addEventListener("mousemove", (e) => {
+    setMouseEnter({
      top : e.clientY,
      left: e.clientX
+   })
    })
   }
 
   return (
-    <div className='homeVideo' 
-    onMouseMove={(e)=> handleMousePostion(e)} >
+    <div className='homeVideo' ref={cursorSec} 
+    onMouseEnter={()=>handleMousePostion()}
+     >
       <div  className="cursorStyling tinyTexts" 
       style={{...mouseEnter}} >
         Watch Reel
