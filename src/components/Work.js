3724e1a357ef9705055d16dba65227cb2dc5990e
naveengitboi <naveenjangiti9@gmail.com>
2023-08-08 20:53:05 +0530

    work page done

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import '../cssComponents/Work.css'
+import mountainImg from '../assets/mountain.webp' 
+import tennisImg from '../assets/tennis.webp'
+const works = [
+    {
+        "workImg":mountainImg,
+        "workHeading":"paragonia",
+        "workDesc":"An Ecommerce experience drive by patagonian's brand mission"
+    },
+    {
+        "workImg":tennisImg,
+        "workHeading":"Wilson",
+        "workDesc":"Wilson — Helping a century-old sports brand find its place in culture and step into an all-new category."
+    },
+    {
+        "workImg":mountainImg,
+        "workHeading":"paragonia",
+        "workDesc":"store.google.com — A long-term relationship committed to bringing the best of Google to people worldwide."
+    }
+]
 
 function Work() {
   return (
@@ -15,6 +34,26 @@ function Work() {
             <div className='hugeHeading hugeHeadingClass'>
                 B/D <div className='rIcon'>R</div>
             </div>
+        </div>
+
+        <div className="works">
+            {
+                works.map((item, index) => (
+                    <div className="worksImageContainer">
+            <div className="imageContainer">
+                <img src={item.workImg} alt="" />
+            </div>
+            <div className="workContent">
+                <h1 className='mediumPara'>
+                {item.workHeading}
+                </h1>
+                <p className='smallPara'>
+                    {item.workDesc}
+                </p>
+            </div>
+        </div>
+                ))
+            }
         </div>
     </div>
   )
