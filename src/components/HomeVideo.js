76c2a34d5ed7324e7b1a12148e4afc7bd148c12e
naveengitboi <naveenjangiti9@gmail.com>
2023-09-22 15:31:32 +0530

    updated home video

--- src/components/HomeVideo.js
+++ src/components/HomeVideo.js
@@ -1,38 +1,44 @@
-import React, { useRef } from 'react'
-import '../cssComponents/HomeVideo.css'
-import videoBg from '../assets/mainVideo.mp4'
-import { useState } from 'react'
-
+import React, { useRef } from "react";
+import "../cssComponents/HomeVideo.css";
+import videoBg from "../assets/mainVideo.mp4";
+import { useState } from "react";
 
 function HomeVideo() {
-  const cursorSec = useRef()
+  const cursorSec = useRef();
   const [mouseEnter, setMouseEnter] = useState({
-    top:0,
-    left:0
-  })
-  const [mouseLeave, setMouseLeave] = useState(false)
-
+    top: 0,
+    left: 0,
+  });
+  const [mouseLeave, setMouseLeave] = useState(false);
 
   const handleMousePostion = (e) => {
-   cursorSec.current.addEventListener("mousemove", (e) => {
-    setMouseEnter({
-     top : e.clientY,
-     left: e.clientX
-   })
-   })
-  }
+    cursorSec.current.addEventListener("mousemove", (event) => {
+      setMouseEnter({
+        top: event.clientY,
+        left: event.clientX,
+      });
+    });
+
+    cursorSec.current.addEventListener("mouseleave", () => {
+      setMouseEnter({
+        top: window.innerHeight/2,
+        left: window.innerWidth/2,
+      });
+    });
+  };
 
   return (
-    <div className='homeVideo' ref={cursorSec} 
-    onMouseEnter={()=>handleMousePostion()}
-     >
-      <div  className="cursorStyling tinyTexts" 
-      style={{...mouseEnter}} >
+    <div
+      className="homeVideo"
+      ref={cursorSec}
+      onMouseEnter={() => handleMousePostion()}
+    >
+      <div className="cursorStyling tinyTexts" style={{ ...mouseEnter }}>
         Watch Reel
       </div>
-        <video src={videoBg} autoPlay loop muted/>
+      <video src={videoBg} autoPlay loop muted />
     </div>
-  )
+  );
 }
 
-export default HomeVideo
+export default HomeVideo;
